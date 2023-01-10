677c44067826782565fbdf3c85a04605bc4b3821
Temilola19 <temiafolabi06@gmail.com>
2023-01-10 17:49:53 +0100

    more

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,38 @@
 import React from "react";
-import { Nav, NavbarContainer, NavLogo } from "./navbarElements";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./NavbarElements";
 
 const Navbar = () => {
   return (
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo>Vell</NavLogo>
+          <NavLogo to="/">Vell</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="About">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="Discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="Signup">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/Signin">Sign In</NavBtnLink>
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
